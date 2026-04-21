3daa423762d6bec1fba93bba70ca0d7a7ba82c25
gdr2305-jpg <gdr2305@gmail.com>
2026-04-21 19:26:43 +0200

    Ajout core config

---
+++ notewise_bullet_generator/core/config.py
@@ -0,0 +1,17 @@
+from reportlab.lib.pagesizes import A5
+from reportlab.lib.units import mm
+
+PAGE_SIZE = A5
+PAGE_WIDTH, PAGE_HEIGHT = A5
+
+MARGIN_MM = 10
+MARGIN = MARGIN_MM * mm
+
+GRID_STEP_MM = 5
+GRID_STEP = GRID_STEP_MM * mm
+
+DOT_RADIUS = 0.5
+LINE_WIDTH = 0.8
+
+FONT = "Helvetica"
+FONT_SIZE = 8
